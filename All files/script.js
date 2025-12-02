4961aa234bcc56c29e019c76d6f903e45575ae61
Angy Hijaze <angy.hijaze@gmail.com>
2025-12-02 19:09:27 +0100

    added reducing calories function

--- All files/script.js
+++ All files/script.js
@@ -26,18 +26,21 @@ function addBudget() {
   if (!budget) return alert("Please enter your daily calories budget");
 
   localStorage.setItem("dailyCaloriesBudget", budget);
-  dailyCalories.textContent = `Daily Calories goal: ${budget}`;
+  dailyCalories.textContent = `Calories Budget: ${budget}`;
   budgetField.value = "";
+}
+
+function resetCalories() {
 }
 
 // a function to capitalize the results for better syntax.
 
 function capitalize(str) {
   return str
-    .toLowerCase()
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+  .toLowerCase()
+  .split(" ")
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(" ");
 }
 
 // Fetching calories and other data via API CALL
@@ -65,22 +68,35 @@ async function checkCalories() {
   // Show results here
   const calories = food.labelNutrients.calories.value;
   const per100 = (calories / food.servingSize) * 100;
-  const rounded = Math.round(per100);
+  let rounded = Math.round(per100);
 
   const resultItem = document.createElement("p");
+
+  remainingBudget -= rounded;
+  localStorage.setItem("caloriesRemained", remainingBudget);
+  dailyCalories.textContent = `Remaining Calories: ${remainingBudget}`
 
   if (!food) {
     resultItem.textContent = "No results found.";
   } else counter++;
   resultItem.textContent = `${counter}. ${capitalize(
     food.description
-  )}: ${rounded} kcal for 100g`;
+  )}: ${rounded} kcal`;
   resultDiv.appendChild(resultItem);
   inputField.value = "";
 }
 
+
+
 // Localstorage
-const savedBudget = localStorage.getItem("dailyCaloriesBudget");
+let savedBudget = localStorage.getItem("dailyCaloriesBudget");
 if (savedBudget) {
-  dailyCalories.textContent = `Daily Calories: ${savedBudget}`;
+  dailyCalories.textContent = `Your Calories for today: ${savedBudget}`;
 }
+
+let remainingBudget = localStorage.getItem("caloriesRemained");
+if (!remainingBudget) {
+  remainingBudget = savedBudget ? Number(savedBudget) : 0;
+  localStorage.setItem("caloriesRemained", remainingBudget);
+}
+dailyCalories.textContent = `Remaining Calories: ${remainingBudget}`;
